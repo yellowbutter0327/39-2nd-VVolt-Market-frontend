1d72b5981579294208c557af3497acfaddf8be68
Jimin Kang <jimin951218@gmail.com>
2022-12-05 15:43:43 +0900

    ADD: login loading page

--- src/pages/Login/KakaoLogin.js
+++ src/pages/Login/KakaoLogin.js
@@ -1,5 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import variables from '../../styles/variables';
 
 export default function KakaoLogin() {
   const navigate = useNavigate();
@@ -21,5 +23,37 @@ export default function KakaoLogin() {
       });
   }, []);
 
-  return <div>로그인 중 입니다.</div>;
+  const completionWord = '로그인 중입니다...';
+  const [loginStatus, setLoginStatus] = useState('');
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const typingInterval = setInterval(() => {
+      setLoginStatus(prevStatusValue => {
+        let result = prevStatusValue
+          ? prevStatusValue + completionWord[count]
+          : completionWord[0];
+        setCount(count + 1);
+        if (count >= completionWord.length) {
+          setCount(0);
+          setLoginStatus('');
+        }
+        return result;
+      });
+    }, 200);
+    return () => {
+      clearInterval(typingInterval);
+    };
+  });
+
+  return <KakaoLoginWrap>{loginStatus}</KakaoLoginWrap>;
 }
+
+const KakaoLoginWrap = styled.div`
+  width: 100%;
+  height: 600px;
+  font-size: 40px;
+  font-weight: bold;
+  color: #882dc4;
+  ${variables.flex('column', 'center', 'center')};
+`;
